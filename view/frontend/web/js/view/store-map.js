e0160b7716b7b4ce5e652ea869904c2f7310b780
Vaimo <vaimo@Vaimos-MBP.lan>
2024-02-13 14:46:07 +0100

    Added info window for markers and separated logic into different functions

--- view/frontend/web/js/view/store-map.js
+++ view/frontend/web/js/view/store-map.js
@@ -9,6 +9,7 @@ define([
       template: "Gustav_Thesis/store-map.html",
       mapCenterLatitude: 59.3293,
       mapCenterLongitude: 18.0686,
+      currentInfowindow: null,
     },
 
     initialize() {
@@ -17,43 +18,72 @@ define([
 
       this.stores = sharedData.stores;
 
-      this.stores.subscribe(
-        function (newStores) {
-          this.showMarkers(newStores);
-        }.bind(this)
-      );
-    },
-
-    initMap: function () {
-      const self = this;
-
-      this.map = new google.maps.Map($("#map")[0], {
-        center: { lat: self.mapCenterLatitude, lng: self.mapCenterLongitude },
-        zoom: 9,
+      this.stores.subscribe((newStores) => {
+        this.clearMarkers();
+        this.showMarkers(newStores);
       });
     },
-    showMarkers(stores) {
-      const self = this;
 
-      if (this.markers) {
-        this.markers.forEach((marker) => {
-          marker.setMap(null);
-        });
-      }
+    initMap() {
+      this.map = new google.maps.Map($("#map")[0], {
+        center: { lat: this.mapCenterLatitude, lng: this.mapCenterLongitude },
+        zoom: 9,
+      });
       this.markers = [];
+    },
 
+    clearMarkers() {
+      this.markers.forEach((marker) => marker.setMap(null));
+      this.markers = [];
+    },
+
+    showMarkers(stores) {
       stores.forEach((store) => {
-        const marker = new google.maps.Marker({
+        let marker = new google.maps.Marker({
           position: {
             lat: parseFloat(store.latitude),
             lng: parseFloat(store.longitude),
           },
-          map: self.map,
+          map: this.map,
           title: store.name,
         });
 
+        marker.addListener("click", () => this.openInfowindow(marker, store));
         this.markers.push(marker);
       });
+    },
+
+    openInfowindow(marker, store) {
+      const formattedPhone = this.formatPhone(store.phone);
+      const content = this.getInfowindowContent(store, formattedPhone);
+
+      if (this.currentInfowindow) {
+        this.currentInfowindow.close();
+      }
+
+      this.currentInfowindow = new google.maps.InfoWindow({ content });
+      this.currentInfowindow.open(this.map, marker);
+    },
+
+    getInfowindowContent(store, formattedPhone) {
+      return `
+        <div class="info-window-container">
+          <h3 class="info-window-name">${store.name}</h3>
+          <p class="info-window-address">${store.address}</p>
+          <div class="info-window-location">
+            <span class="location-city">${store.city},</span>
+            <span class="location-postcode">${store.postcode},</span>
+            <span class="location-country">${store.country}</span>
+          </div>
+          <div class="info-window-contact">
+            <p class="contact-hours">${store.hours}</p>
+            <p class="contact-phone">${formattedPhone}</p>
+          </div>
+        </div>`;
+    },
+
+    formatPhone(phone) {
+      return phone.replace(/^(08|07\d)/, "$1-");
     },
   });
 });
